c8d29b7f45f8ddfcf2559c07f6ee8b666095f983
ALATAF ANSARI <alataf.dce.cs1@gmail.com>
2023-03-13 01:51:34 +0530

    Create goal parser interpretation of string in java

---
+++ Programming Skills/Programming-Skills-I/Day-07(String)/Goal Parser Interpretation.java
@@ -0,0 +1,54 @@
+/******************************************************************************************************************************************************
+-------------------------
+level- Easy
+Question no-1678
+-------------------------
+
+You own a Goal Parser that can interpret a string command. The command consists of an alphabet of "G", "()" and/or "(al)" in some order. 
+The Goal Parser will interpret "G" as the string "G", "()" as the string "o", and "(al)" as the string "al". The interpreted strings are then
+concatenated in the original order.
+
+Given the string command, return the Goal Parser's interpretation of command.
+
+---------------------------------------------------------------------------------------
+
+Example 1:
+
+Input: command = "G()(al)"
+Output: "Goal"
+Explanation: The Goal Parser interprets the command as follows:
+G -> G
+() -> o
+(al) -> al
+The final concatenated result is "Goal".
+Example 2:
+
+Input: command = "G()()()()(al)"
+Output: "Gooooal"
+Example 3:
+
+Input: command = "(al)G(al)()()G"
+Output: "alGalooG"
+
+----------------------------------------------------------------------------------------
+
+*******************************************************************************************************************************************************/
+
+class Solution {
+    public String interpret(String command) {
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0;i<command.length();i++){
+            if(command.charAt(i)=='G'){
+                sb.append("G");
+            }
+            if(command.charAt(i)=='(' && command.charAt(i+1)==')'){
+                sb.append("o");
+            }
+            if(command.charAt(i)=='(' && command.charAt(i+1)=='a'){
+                sb.append("al");
+            }
+        }
+        
+        return sb.toString();
+    }
+}
